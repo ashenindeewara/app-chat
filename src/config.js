15c8248e467aa94cfaa529f04fc12beae9c19164
Ashen Indeewara <69989225+ashenindeewara@users.noreply.github.com>
2023-08-30 01:36:15 +0530

    Update config.js

--- src/config.js
+++ src/config.js
@@ -3,6 +3,6 @@ import { config } from 'dotenv'
 config()
 
 export const PORT = process.env.PORT || 4000
-export const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/app-chat';
+export const MONGODB_URI = process.env.MONGODB_URI || 'mongodb+srv://kasunlakshan3368:7iwzQuWcBelzB9OU@whatsappbot.skbtrbg.mongodb.net/?retryWrites=true&w=majority';
 export const SECRET = process.env.SECRET
 export const CLIENT_PORT = process.env.CLIENT_PORT || 'http://localhost:5173'
